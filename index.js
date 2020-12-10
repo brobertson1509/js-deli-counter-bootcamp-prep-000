27d40f6eae9431080e85debe084276a3c96ef587
dandy-collaborator-4866 <brobertson@utexas.edu>
2020-12-10 15:54:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var katzDeliLine = [];
 
 function takeANumber(number, name){
+  katzDeliLine.push('${name}');
     katzDeliLine.push(`Welcome, ${name}. You are number ${number + 1} in line.`);
   return katzDeliLine;
 }
